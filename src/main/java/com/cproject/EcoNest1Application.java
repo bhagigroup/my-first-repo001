9b223047cc3bd5ab8e3378ec886a6f5170cfb429
Naresh <naresh@gosure.ai>
2025-07-12 13:26:32 +0530

    first commit

---
+++ src/main/java/com/cproject/EcoNest1Application.java
@@ -0,0 +1,13 @@
+package com.cproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoNest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoNest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cproject/EcoNest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoNest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
